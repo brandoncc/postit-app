66e50d713c60f62aadf0fd55819501a7139fdcad
Brandon Conway <brandoncc@gmail.com>
2014-01-04 21:09:51 -0800

    create category model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140105045307) do
+ActiveRecord::Schema.define(version: 20140105050817) do
+
+  create_table "categories", force: true do |t|
+    t.string "name"
+  end
 
   create_table "comments", force: true do |t|
     t.text    "body"
