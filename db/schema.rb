41eb1eda2c9bcf825dca86760f692379cafdf7a7
Brandon Conway <brandoncc@gmail.com>
2014-01-06 11:26:51 -0800

    add timestamps and setup post_categories join table and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140105050817) do
+ActiveRecord::Schema.define(version: 20140106190452) do
 
   create_table "categories", force: true do |t|
-    t.string "name"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "comments", force: true do |t|
     t.text    "body"
     t.integer "user_id"
     t.integer "post_id"
+  end
+
+  create_table "post_categories", force: true do |t|
+    t.integer  "category_id"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "posts", force: true do |t|
@@ -33,7 +42,9 @@ ActiveRecord::Schema.define(version: 20140105050817) do
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: true do |t|
-    t.string "username"
+    t.string   "username"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
