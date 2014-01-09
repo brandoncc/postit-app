702a4d9d48e5503ae0a923e5360043be7158bc79
Brandon Conway <brandoncc@gmail.com>
2014-01-09 09:26:57 -0800

    associate selected categories to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    set_selected_categories
 
     if @post.save
       redirect_to post_path(@post.id)
@@ -29,12 +30,22 @@ class PostsController < ApplicationController
 
   def update
     @post.update_attributes(post_params)
+    set_selected_categories
     @post.save
 
     redirect_to(post_path(@post))
   end
 
   private
+  def set_selected_categories
+    selected_categories = params[:post][:categories]
+    if selected_categories.nil?
+      @post.category_ids = []
+    else
+      @post.category_ids = params[:post][:categories].map { |c| c.to_i }
+    end
+  end
+
   def set_post
     @post = Post.find(params[:id])
   end
