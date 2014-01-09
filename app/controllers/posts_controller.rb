11c69fa07aaa585f6ffad664a301722fefcf04f5
Brandon Conway <brandoncc@gmail.com>
2014-01-08 20:33:24 -0800

    add post actions/validations/views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,35 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to post_path(@post.id)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update_attributes(post_params)
+    @post.save
+
+    render :show
+  end
+
+  private
+  def post_params
+    params[:post].permit(:title, :url, :description)
+  end
 end
