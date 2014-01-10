116da4593d83cbc83532a4d1742d25137b334539
Brandon Conway <brandoncc@gmail.com>
2014-01-09 23:44:45 -0800

    add timestamps to comments and posts tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140106190452) do
+ActiveRecord::Schema.define(version: 20140109195602) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -20,9 +20,11 @@ ActiveRecord::Schema.define(version: 20140106190452) do
   end
 
   create_table "comments", force: true do |t|
-    t.text    "body"
-    t.integer "user_id"
-    t.integer "post_id"
+    t.text     "body"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "post_categories", force: true do |t|
@@ -33,10 +35,12 @@ ActiveRecord::Schema.define(version: 20140106190452) do
   end
 
   create_table "posts", force: true do |t|
-    t.string  "url"
-    t.string  "title"
-    t.text    "description"
-    t.integer "user_id"
+    t.string   "url"
+    t.string   "title"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
