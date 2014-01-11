f7a4c8ae7c26caf321ea2208eb5f8eb8452f7f67
Brandon Conway <brandoncc@gmail.com>
2014-01-11 12:32:47 -0800

    setup authentication

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:notice] = 'Your account has been created.'
+      redirect_to posts_path
+    else render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      flash[:notice] = 'Your account has been updated.'
+      redirect_to user_path(params[:id])
+    else render :edit
+    end
+  end
+
+  def show
+    @user = User.find(1)
+    @tab = params[:tab].nil? ? 'posts' : 'comments'
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
+
+  has_secure_password validations: false
+
+  validates :username, presence: true
+  validates :password, presence: true, confirmation: true, length: { minimum: 5 }
 end
