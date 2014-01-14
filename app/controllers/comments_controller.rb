db966f68f98829ad5b1be7f614b978ec37b9b434
Brandon Conway <brandoncc@gmail.com>
2014-01-14 13:16:00 -0800

    fix @user bug

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
-    @comment.creator = @user
+    @comment.creator = current_user
 
     if @comment.save
       flash[:notice] = 'Your comment was added.'
