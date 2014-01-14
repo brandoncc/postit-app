cdf05a27e6db1e7d1210fb1463955bd581d4667c
Brandon Conway <brandoncc@gmail.com>
2014-01-14 10:07:22 -0800

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140111202007) do
+ActiveRecord::Schema.define(version: 20140113233612) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -45,17 +45,20 @@ ActiveRecord::Schema.define(version: 20140111202007) do
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
-  create_table "sessions", force: true do |t|
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "users", force: true do |t|
     t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+  end
+
+  create_table "votes", force: true do |t|
+    t.integer  "value"
+    t.integer  "voteable_id"
+    t.string   "voteable_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
 end
