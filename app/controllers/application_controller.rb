6d712992b431d96657858d1456addaa9b7d82454
Brandon Conway <brandoncc@gmail.com>
2014-01-14 09:59:07 -0800

    rewrite logged in helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :set_current_user
+  helper_method :logged_in?, :current_user
 
-  def set_current_user
-    return nil unless session[:user_id]
-    @current_user ||= User.find(session[:user_id])
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def require_login
-    redirect_to login_path if @current_user.nil?
+    redirect_to login_path unless logged_in?
   end
 end
