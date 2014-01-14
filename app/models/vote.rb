0e81fd323408b50f1d51c8f9315da210c1d0a136
Brandon Conway <brandoncc@gmail.com>
2014-01-14 10:13:02 -0800

    refer to user as creator

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,6 @@
 class Vote < ActiveRecord::Base
   belongs_to :voteable, polymorphic: true
-  belongs_to :user
+  belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
 
   validates_uniqueness_of :user_id, scope: [:voteable_id, :voteable_type]
 end
