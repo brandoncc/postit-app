68439ddddac0a0b3c15dd939875327c5c3be2bb0
Brandon Conway <brandoncc@gmail.com>
2014-01-14 14:04:05 -0800

    use set_post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :require_login, except: [:index, :show]
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :vote]
 
   def index
     @posts = Post.all
