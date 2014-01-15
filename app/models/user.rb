477960525a98c260513cc1ef54f707ddc751aa4d
Brandon Conway <brandoncc@gmail.com>
2014-01-15 15:46:37 -0800

    sluggify users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,29 @@ class User < ActiveRecord::Base
 
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, confirmation: true, length: { minimum: 5 }, on: :create
+
+  before_save :generate_slug
+
+  def generate_slug
+    potential_slug = self.username.downcase.gsub(/\s+/, '-').gsub(/[^A-Za-z0-9-]/, '')
+
+    index = 1
+
+    until User.find_by(slug: potential_slug).nil?
+
+      if index == 1
+        potential_slug += "-#{index}"
+      else
+        potential_slug.gsub!(/-\d+$/, "-#{index}")
+      end
+
+      index += 1
+    end
+
+    self.slug = potential_slug
+  end
+
+  def to_param
+    self.slug
+  end
 end
