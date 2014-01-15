c86e6b8a0e08e0d861f789442cb15450b6810bc6
Brandon Conway <brandoncc@gmail.com>
2014-01-14 16:54:57 -0800

    use set_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_login, except: [:new, :create]
+  before_action :set_user, only: [:show, :edit, :update]
 
   def new
     @user = User.new
@@ -17,12 +18,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+
   end
 
   def update
-    @user = User.find(params[:id])
-
     if @user.update(user_params)
       flash[:notice] = 'Your account has been updated.'
       redirect_to user_path(params[:id])
@@ -32,12 +31,15 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @tab  = params[:tab].nil? ? 'posts' : 'comments'
   end
 
   private
   def user_params
     params.require(:user).permit(:username, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
