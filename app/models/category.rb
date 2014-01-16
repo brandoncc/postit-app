a509079f3516e4c0f717f22cf59e15337af3fa55
Brandon Conway <brandoncc@gmail.com>
2014-01-15 16:18:51 -0800

    sluggify categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,28 @@ class Category < ActiveRecord::Base
   has_many :posts, through: :post_categories
 
   validates_presence_of :name
+
+  before_save :generate_slug
+
+  def generate_slug
+    potential_slug = self.name.downcase.gsub(/\s+/, '-').gsub(/[^A-Za-z0-9-]/, '')
+
+    index = 1
+
+    until Category.find_by(slug: potential_slug).nil?
+      if index == 1
+        potential_slug += "-#{index}"
+      else
+        potential_slug.gsub!(/-\d+$/, "-#{index}")
+      end
+
+      index += 1
+    end
+
+    self.slug = potential_slug
+  end
+
+  def to_param
+    self.slug
+  end
 end
